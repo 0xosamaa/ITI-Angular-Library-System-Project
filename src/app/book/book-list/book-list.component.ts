a6312091730e5391059c77debdb3733747743844
amrbasiony97 <amrbasiony97@gmail.com>
2023-04-09 19:48:26 +0200

    Delete Book

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -1,3 +1,5 @@
+import { ConfirmEventType, MessageService } from 'primeng/api';
+import { ConfirmationService } from 'primeng/api';
 import { BookService } from './../../services/book.service';
 import { Component, OnInit } from '@angular/core';
 import { Table } from 'primeng/table';
@@ -14,7 +16,11 @@ export class BookListComponent implements OnInit {
   books: Book[] = [];
   book: Book = new Book();
 
-  constructor(public bookService: BookService) { }
+  constructor(
+    public bookService: BookService,
+    public confirmationService: ConfirmationService,
+    public messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
     this.loading = false;
@@ -42,5 +48,44 @@ export class BookListComponent implements OnInit {
     this.detailsVisible = true;
     this.book = book;
     console.log(this.book);
+  }
+
+  deleteDialog(_id: string) {
+    this.confirmationService.confirm({
+      message: 'Are you sure to delete book?',
+      header: 'Delete Book',
+      icon: 'pi pi-info-circle',
+      accept: () => {
+        this.bookService.deleteBook(_id).subscribe(
+          (data:any) => {
+            this.messageService.add({ severity: 'info', summary: 'Confirmed', detail: 'Book deleted successfully' });
+            this.bookService.getBooks().subscribe(
+              (data:any) => {
+                this.books = data.data;
+                console.log(this.books);
+              },
+              (error:any) => {
+                console.log(error.error.message);
+              }
+            );
+          },
+          (error) => {
+            console.log(error.error.message);
+          }
+        );
+
+
+      },
+      reject: (type: any) => {
+        switch (type) {
+          case ConfirmEventType.REJECT:
+            this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'Book Deletion rejected' });
+            break;
+          case ConfirmEventType.CANCEL:
+            this.messageService.add({ severity: 'warn', summary: 'Cancelled', detail: 'Book Deletion cancelled' });
+            break;
+        }
+      }
+    });
   }
 }
