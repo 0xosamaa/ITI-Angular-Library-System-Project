233ead00f9c0236d59b9a60703b796b57fb22484
amrbasiony97 <amrbasiony97@gmail.com>
2023-04-14 07:06:02 +0200

    Added validation to book add

--- src/app/book/add-book/add-book.component.ts
+++ src/app/book/add-book/add-book.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { BookService } from './../../services/book.service';
 import { Book } from './../../_models/book';
@@ -9,31 +10,66 @@ import { Book } from './../../_models/book';
   styleUrls: ['./add-book.component.css']
 })
 export class AddBookComponent {
-  book: Book = new Book();
+  bookForm: FormGroup;
   visible: boolean = false;
+  submitted = false;
 
-  constructor(public bookService: BookService) {}
+  constructor(public bookService: BookService, private formBuilder: FormBuilder) {
+    this.bookForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      author: ['', Validators.required],
+      publisher: ['', Validators.required],
+      datePublished: ['', Validators.required],
+      category: ['', Validators.required],
+      pagesCount: [1, [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(1800)
+      ]],
+      copiesCount: [1, [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(100)
+      ]],
+      isAvailable: new FormControl(true),
+      shelfNo: [1, [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(100)
+      ]]
+    });
+  }
 
   showAddDialog() {
     this.visible = true;
   }
 
+  onDialogHide() {
+    this.submitted = false;
+    this.bookForm.reset();
+  }
+
   add() {
-    this.book.dateAdded = new Date();
-    this.visible = false;
-    this.bookService.addBook(new Book(
-      this.book._id,
-      this.book.title,
-      this.book.author,
-      this.book.publisher,
-      this.book.dateAdded,
-      this.book.datePublished,
-      this.book.category,
-      this.book.pagesCount,
-      this.book.copiesCount,
-      this.book.isAvailable,
-      this.book.shelfNo
-    ));
-    this.book = new Book();
+    this.submitted = true;
+    if (this.bookForm.valid) {
+      let dateAdded = new Date();
+      const formValues = this.bookForm.value;
+      const book = new Book(
+        "",
+        formValues.title,
+        formValues.author,
+        formValues.publisher,
+        dateAdded,
+        formValues.datePublished,
+        formValues.category,
+        formValues.pagesCount,
+        formValues.copiesCount,
+        formValues.isAvailable,
+        formValues.shelfNo
+      );
+      this.visible = false;
+      this.submitted = false;
+      this.bookService.addBook(book);
+    }
   }
 }
--- src/app/book/update-book/update-book.component.ts
+++ src/app/book/update-book/update-book.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { BookService } from 'src/app/services/book.service';
 import { Book } from 'src/app/_models/book';
 
